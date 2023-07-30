11b957f9d1a81b64fe6f19df49a9a0e0033beba7
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-07-30 21:22:15 +0900

    good

--- compose/navigation/app/src/main/java/com/bestswlkh0310/navigation/MainActivity.kt
+++ compose/navigation/app/src/main/java/com/bestswlkh0310/navigation/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.bestswlkh0310.navigation
 
+import androidx.compose.runtime.remember
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import com.bestswlkh0310.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,26 +30,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    App()
+                    NavigationGraph()
                 }
             }
         }
     }
 }
 
-@Composable
-fun App() {
-
-}
-
 enum class NAV_ROUTE(val routeName: String, val description: String) {
-    HOME("제가 누군지 아시나요??", "'나'는 누구인가"),
-    IDK("내가 어떻게 알아 이 자식아", "정말 모르겠어요")
+    HOME("HOME", "'나'는 누구인가"),
+    IDK("IDK", "정말 모르겠어요")
 }
 
 class RouteAction(navHostController: NavHostController) {
     val navTo: (NAV_ROUTE) -> Unit = {
-        navHostController.navigate(it)
+        navHostController.navigate(it.routeName)
     }
 
     val goBack: () -> Unit = {
@@ -52,12 +53,58 @@ class RouteAction(navHostController: NavHostController) {
 }
 
 @Composable
-fun NavigationGraph(startRoute: String = NAV_ROUTE = NAV_ROUTE.HOME) {
+fun NavigationGraph(startRoute: NAV_ROUTE = NAV_ROUTE.HOME) {
     val navController = rememberNavController()
+    val routeAction = remember(navController) { RouteAction(navController) }
 
-    val routeAction = remember(navController) {
-        NavHost(navController, startRoute) {
-            
+    Log.d("TAG", "${NAV_ROUTE.HOME.routeName} - NavigationGraph() called")
+    NavHost(navController, startRoute.routeName) {
+        composable(NAV_ROUTE.HOME.routeName) {
+            HomeView(routeAction = routeAction)
+        }
+        composable(NAV_ROUTE.IDK.routeName) {
+            IdkScreen(routeAction = routeAction)
+        }
+    }
+}
+
+
+@Composable
+fun HomeView(routeAction: RouteAction){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(Modifier.padding(16.dp)) {
+            NavButton(route = NAV_ROUTE.IDK, routeAction = routeAction)
+        }
+    }
+}
+
+@Composable
+fun ColumnScope.NavButton(route: NAV_ROUTE, routeAction: RouteAction){
+    Button(onClick = {
+        routeAction.navTo(route)
+    },colors = ButtonDefaults.buttonColors(contentColor = Color.Black),
+        modifier = Modifier
+            .weight(1f)
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = route.description,
+            style = TextStyle(Color.White, 22.sp, FontWeight.Medium)
+        )
+    }
+}
+
+@Composable
+fun IdkScreen(routeAction: RouteAction){
+    Surface(Modifier.fillMaxSize()) {
+        Box(Modifier.padding(8.dp), Alignment.Center){
+            Text(text = "로그인 화면", style = TextStyle(Color.Black, 22.sp, FontWeight.Medium))
+            Button(onClick = routeAction.goBack,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .offset(y = 100.dp)) {
+                Text("뒤로가기")
+            }
         }
     }
 }
@@ -67,6 +114,6 @@ fun NavigationGraph(startRoute: String = NAV_ROUTE = NAV_ROUTE.HOME) {
 @Composable
 fun GreetingPreview() {
     NavigationTheme {
-        App()
+        NavigationGraph()
     }
 }
